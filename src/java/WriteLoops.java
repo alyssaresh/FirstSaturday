1a832736763fd15dabe7270be45283200f743c2f
Alyssa Resh <aresh@zipcoders-mbp.mynetworksettings.com>
2024-10-31 22:27:09 -0400

    finished some loops

--- src/java/WriteLoops.java
+++ src/java/WriteLoops.java
@@ -14,92 +14,97 @@ public class WriteLoops {
 
     public int oneToFive() {
         int w = 0;
-
-        // Write a FOR loop that counts from 1 to 10.
-            // calling
-            w = w + 1;
-            // each time through the loop
-
-        // this will tell the test how many times the loop executed.
-        return w;
-    }
-
-    public int oneToTen() {
-        int w = 0;
-
+        for (int i = 1; i<=5; i++) {
         // Write a FOR loop that counts from 1 to 10.
         // calling
         w = w + 1;
         // each time through the loop
-        
+        // this will tell the test how many times the loop executed.
+    } return w;
+    }
+    public int oneToTen() {
+        int w = 0;
+        for (int i = 1; i<=10; i++) {
+            // Write a FOR loop that counts from 1 to 10.
+            // calling
+            w = w + 1;
+            // each time through the loop
+        }
         return w;
     }
 
     public int startAtTwentyOne() {
         int w = 0;
-
-        // Write a FOR loop that makes 10 iterations, start at 21.
-        // calling
-        w = w + 1;
+        for (int i = 21; i <= 31; i++) {
+            // Write a FOR loop that makes 10 iterations, start at 21.
+            // calling
+            w = w + 1;
         // each time through the loop
-        
+    }
         return w;
     }
 
     public int countDown() {
         int w = 0;
-
-        // Write a FOR loop that counts down from 100 to 0.
-        // calling
-        w = w + 1;
-        // each time through the loop
-        
+        for (int i = 100; i >0; i--) {
+            // Write a FOR loop that counts down from 100 to 0.
+            // calling
+            w = w + 1;
+            // each time through the loop
+        }
         return w;
     }
 
     public int byTwoTo32() {
         int w = 0;
-
-        // Write a FOR loop from 0 to 32 by 2s.
-        // calling
-        w = w + 1;
-        // each time through the loop
+        for (int i = 0; i <= 32; i+=2) {
+            // Write a FOR loop from 0 to 32 by 2s.
+            // calling
+            w = w + 1;
+            // each time through the loop
+        }
         return w;
     }
 
     public int countDownFrom5000() {
         int w = 0;
-
-        // Write a FOR loop from 1 to less than 5001 by 11s.
-        // calling
-        w = w + 1;
+        for (int i = 1; i < 5001; i += 11){
+            // Write a FOR loop from 1 to less than 5001 by 11s.
+            // calling
+            w = w + 1;
         // each time through the loop
-        
+    }
         return w;
     }
 
     public int nestedFors() {
         int w = 0;
-
-        // Write a nested FOR loop(s), where one counts from
-        // 0 to less than 20 and the inner one counts from 0 to 4
-                // calling
+        for (int i = 0; i < 20; i++) {
+            for (int j = 0; j <= 4; j++) {
                 w = w + 1;
-                // each time through the inner loop
-
-        return w;
+            }
+            // Write a nested FOR loop(s), where one counts from
+            // 0 to less than 20 and the inner one counts from 0 to 4
+            // calling
+            // each time through the inner loop
+        } return w;
     }
 
     public int helloZipCode() {
         int w = 0;
+        for (int i = 5; i <= 105; i++) {
+            if (i > 51) {
+                System.out.print("Hello Zipcode");
+            } else w = w + 1;
 
+        }
         // Write a FOR loop that counts from 5 to 105. Put an IF
         // statement inside the loop that checks the
         // loop index counter and if it’s greater than 51,
         // prints “Hello Zipcode” instead of the statement w = w + 1;
 
                 // calling
-                w = w + 1;
+
             // each time through the inner loop
         
         return w;
@@ -121,6 +126,8 @@ public class WriteLoops {
             i = i - 1;
         } while (i > 0);
         // what's the primary difference between them?!?
+        //a do while loop's condition is checked AFTER running the loop, so it will run at least once.
+        //a while loop's condition is checked before the loop
     }
 
     // Write a WHILE loop that checks “gpsCurrentLocation()”
@@ -128,11 +135,13 @@ public class WriteLoops {
     // After the loop is done, print “Honey, I’m Home!”
     public int driveHome() {
         int w = 0;
-
+        while (!gpsCurrentLocation().equals("Home")) {
+            driveSomeMore();
+            w = w + 1;
+        }
         // you need to use a .equals for two Strings.
 
             // calling
-            w = w + 1;
             // each time through the inner loop
         
 
